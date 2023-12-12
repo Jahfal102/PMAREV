4a6b0b54f4c8bf70e6b98909f2314de84e73f89b
Jahfal102 <126544993+Jahfal102@users.noreply.github.com>
2023-12-12 21:11:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 import streamlit as st
-from keras.models import load_model
+from keras.optimizers import Adam
 from PIL import Image
 import numpy as np
+from tensorflow.keras.models import load_model
+
 
 from util import classify, set_background
 
-
+# Set background
 set_background('./BG/bg.jpg')
 
-# set title
+# Set title
 st.title('Pneumonia covid classification')
 
-# set header
+# Set header
 st.header('Please upload a chest X-ray image')
 
-# upload file
+# Upload file
 file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 
-# load classifier
-model = load_model('.model/PMA3.h5')
+# Load classifier
+model = load_model('./model/PMA3.h5')
 
-# load class names
+# Compile the model
+optimizer = Adam(lr=0.01)  # Adjust the learning rate as needed
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Load class names
 with open('./model/labels.txt', 'r') as f:
     class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
     f.close()
 
-# display image
+# Display image
 if file is not None:
     image = Image.open(file).convert('RGB')
     st.image(image, use_column_width=True)
 
-    # classify image
+    # Classify image
     class_name, conf_score = classify(image, model, class_names)
 
-    # write classification
+    # Write classification
     st.write("## {}".format(class_name))
     st.write("### score: {}%".format(int(conf_score * 1000) / 10))
